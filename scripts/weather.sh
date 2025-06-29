642449b490762fd8bfb86ac216877f759f061c35
thomasgmg <cabralzinho.mg@gmail.com>
2025-06-29 10:11:11 +0200

    change the wind output a bit

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -3,5 +3,5 @@
 # Fetch weather data for Mettmann
 # weather=$(curl -s 'wttr.in/Mettmann?format=2' | tr -d ' ' | tr -d '+' | tr -d '🌬️ ')
 # weather=$(curl -s 'wttr.in/Mettmann?format=2' | tr -d ' ' | tr -d '+' | sed 's/🌬️//')
-weather=$(curl -s 'wttr.in/Mettmann?format=2' | tr -d ' ' | tr -d '+' | sed 's/🌬️/   /' | sed 's/→//')
+weather=$(curl -s 'wttr.in/Mettmann?format=2' | tr -d ' ' | tr -d '+' | sed 's/🌬️/   /')
 echo -e "$weather"
