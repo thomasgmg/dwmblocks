1d08c1a53594d5797bff0e11ec28771e01447a42
thomasgmg <cabralzinho.mg@gmail.com>
2025-08-01 09:59:42 +0200

    Update weather.sh

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 # Fetch weather data for Mettmann
-# weather=$(curl -s 'wttr.in/Mettmann?format=2' | tr -d ' ' | tr -d '+' | tr -d '🌬️ ')
-# weather=$(curl -s 'wttr.in/Mettmann?format=2' | tr -d ' ' | tr -d '+' | sed 's/🌬️//')
-weather=$(curl -s 'wttr.in/Mettmann?format=2' | tr -d ' ' | tr -d '+' | sed 's/🌬️/   /')
+weather=$(curl -s 'wttr.in/Silveira?format=2' | tr -d ' ' | tr -d '+' | sed 's/🌬️/   /')
 echo -e "$weather"
