d3c0476108e657fab4c530476b3a584bd220fce7
TMG <thomas@voidthomas.lan>
2025-09-02 13:52:35 +0000

    remove spaces next to icons for cleaner look

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-weather=$(curl -s 'wttr.in/Silveira?format=1' | tr -d '+' | tr -d ' ' | sed 's/☀️/ /' | sed 's/⛅️/ /' | sed 's/🌦 / /' | sed 's/☁️/ /')
+weather=$(curl -s 'wttr.in/Silveira?format=1' | tr -d '+' | tr -d ' ' | sed 's/☀️/ /' | sed 's/⛅️/ /' | sed 's/🌦/ /' | sed 's/☁️/ /')
 echo -e "$weather"
