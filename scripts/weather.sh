4daf4d864d25b5237e0aea6fd477fb73c5823c0d
TMG <thomas@voidthomas.lan>
2025-12-07 13:39:14 +0000

    add wind to weather information

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-weather=$(curl -s 'wttr.in/Silveira?format=1' | tr -d '+' | tr -d ' ' | sed 's/☀️/ /' | sed 's/⛅️/ /' | sed 's/🌦/ /' | sed 's/☁️/ /' | sed 's/🌫/ /' | sed 's/🌩/󰖓 /' | sed 's/🌧/ /')
+weather=$(curl -s 'wttr.in/Silveira?format=2' | tr -d '+' | tr -d ' ' | sed 's/🌬️/ /' | sed 's/☀️/ /' | sed 's/⛅️/ /' | sed 's/🌦/ /' | sed 's/☁️/ /' | sed 's/🌫/ /' | sed 's/🌩/󰖓 /' | sed 's/🌧/ /')
 echo -e "$weather"
