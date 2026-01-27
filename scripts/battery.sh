5552a4fa8061509f36431c83882f50ff49ed8630
TMG <thomas@voidthomas.lan>
2026-01-27 21:48:46 +0000

    change icon

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-awk '{cap=$1; print (cap<25?" ":cap<50?" ":cap<75?" ":" ") cap"%"}' /sys/class/power_supply/BAT0/capacity
+awk '{cap=$1; print (cap<25?"󰂎 ":cap<50?"󱊡 ":cap<75?"󱊢 ":"󱊣 ") cap"%"}' /sys/class/power_supply/BAT0/capacity
