411f9e490e73ce95c692f671d2281753d99ab70b
TMG <thomas@voidthomas.lan>
2026-01-30 10:23:15 +0000

    refactor

--- scripts/bitcoin.sh
+++ scripts/bitcoin.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-printf " %s€" "$(curl -sf "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=eur" 2>/dev/null \
-                 | jq -r '.bitcoin.eur // "0"' \
-                 | cut -f1 -d. \
-                 | sed ':a;s/\B[0-9]\{3\}\>/.&/;ta')" || printf " --"
+if ! price=$(curl -sSf "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=eur" 2>/dev/null | jq -r '.bitcoin.eur // "0"'); then
+    printf "err 󱚵 "
+    exit 0
+fi
 
+# Format integer part with European thousand separators (dots)
+formatted=$(printf "%.0f" "$price" | sed ':a;s/\B[0-9]\{3\}\>/.&/;ta')
+
+printf "%s€" "$formatted"
