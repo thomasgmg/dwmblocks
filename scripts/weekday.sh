a97c295852c670d55205765c8a92561a46c489f1
TMG <thomas@voidthomas.lan>
2026-02-09 15:02:39 +0000

    refactor

--- scripts/weekday.sh
+++ scripts/weekday.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 week=$(date '+%V')
-if [ $((week % 2)) -eq 0 ]; then
-    echo "Par ($week)"
-else
-    echo "Impar ($week)"
-fi
+# if [ $((week % 2)) -eq 0 ]; then
+#     echo "Par ($week)"
+# else
+#     echo "Impar ($week)"
+# fi
+
+echo "$week"
