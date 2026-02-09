c0572676cd423850dc900935a752f4e8df4e4e4a
TMG <thomas@voidthomas.lan>
2026-02-09 15:02:28 +0000

    change currency to usd

--- scripts/bitcoin.sh
+++ scripts/bitcoin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if ! price=$(curl -sSf "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=eur" 2>/dev/null | jq -r '.bitcoin.eur // "0"'); then
+if ! price=$(curl -sSf "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd" 2>/dev/null | jq -r '.bitcoin.usd // "0"'); then
     printf "err 󱚵 "
     exit 0
 fi
@@ -8,4 +8,4 @@ fi
 # Format integer part with European thousand separators (dots)
 formatted=$(printf "%.0f" "$price" | sed ':a;s/\B[0-9]\{3\}\>/.&/;ta')
 
-printf "%s" "$formatted"
+printf "%sK" "$formatted"
